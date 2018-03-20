a1788fad1954ef361f53397ddafdf1acb39b0477
Clay Terry <lclaytont@gmail.com>
2018-03-20 21:34:17 +0000

    Second for June

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319231456) do
+ActiveRecord::Schema.define(version: 20180320212715) do
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
